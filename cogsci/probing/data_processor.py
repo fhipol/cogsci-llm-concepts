9a6b49f4df92aa0301d9dbc2f00ec189663d38bf
fhipol <fernando.jesus.garcia.hipola@vinted.com>
2024-07-17 00:14:49 +0200

    import fix

--- cogsci/probing/data_processor.py
+++ cogsci/probing/data_processor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
